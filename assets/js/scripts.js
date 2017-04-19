565d44b992243cd765604104e43228dd6e7d48c6
sergiownd <sergiogonzalezmejias@MacBook-Pro-de-Sergio.local>
2017-04-19 16:34:09 +0200

    added contacto funcionality

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-     $('.carousel').carousel({
+    $('.carousel').carousel({
         interval: false
     }); 
+
+    $('.postal').click(function(){
+    	$('.postal').each(function(){
+    		$(this).removeClass('active');
+    	});
+    	$(this).addClass('active');
+    	$('.postal_saved').val($(this).children().attr('src'));
+    })
 })
    
